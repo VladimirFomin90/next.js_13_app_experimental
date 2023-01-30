770d9059babe83637c9b856b93e0903adb3c9f8a
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-01-30 21:49:30 +0600

    init next.js_13 experimental app

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import './globals.css';
+import { Noto_Sans } from '@next/font/google';
+
+const noto_sans = Noto_Sans({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    variable: '--font-family',
+});
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+            <head />
+            <body className={`${noto_sans.className}`}>{children}</body>
+        </html>
+    );
+}
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <main>
+            <h1 className="text-lg py-2">Next.js 13 developer</h1>
+        </main>
+    );
+}
